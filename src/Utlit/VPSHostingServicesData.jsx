7875a83307035b1ec332044c33a825405fb731f2
Mostafizur <mostafizurdh2000@gmail.com>
2023-07-14 00:02:35 +0600

    bug fix 7

--- src/Utlit/VPSHostingServicesData.jsx
+++ src/Utlit/VPSHostingServicesData.jsx
@@ -1,6 +1,11 @@
 import newZealand from "../assets/images/new-zealand.svg";
 import cpu from "../assets/images/cpu.svg";
 import ramMemory from "../assets/images/ram-memory.svg";
+import hardDisk from "../assets/images/hard-disk.svg";
+import speedometer from "../assets/images/speedometer.svg";
+import cable from "../assets/images/cable.svg";
+import priceTag from "../assets/images/price-tag.svg";
+
 export const VPSHostingServicesData = [
   {
     id: 1,
@@ -26,19 +31,38 @@ export const VPSHostingServicesData = [
         name: "64GB",
         subName: "up to 384GB",
       },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
+      },
     ],
   },
   {
     id: 2,
-    isfullManaged: true,
+    fullManaged: true,
     title: {
       titleName: "INTEL GOLD 7402P",
-      titleIcon: "",
+      titleIcon: newZealand,
       titleTookit: "Eu-Nord-1:3 in stock",
     },
     subTitle: {
       subTitleName: "Middleweight",
-      subTitleIcon: "",
+      subTitleIcon: newZealand,
       subTitleTookit: "Eu-Nord-1:3 in stock",
     },
     hostingInfo: [
@@ -51,6 +75,25 @@ export const VPSHostingServicesData = [
         icon: ramMemory,
         name: "64GB",
         subName: "up to 384GB",
+      },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
       },
     ],
   },
@@ -59,12 +102,12 @@ export const VPSHostingServicesData = [
     fullManaged: true,
     title: {
       titleName: "INTEL GOLD 7402P",
-      titleIcon: "",
+      titleIcon: newZealand,
       titleTookit: "Eu-Nord-1:3 in stock",
     },
     subTitle: {
       subTitleName: "Middleweight",
-      subTitleIcon: "",
+      subTitleIcon: newZealand,
       subTitleTookit: "Eu-Nord-1:3 in stock",
     },
     hostingInfo: [
@@ -77,6 +120,25 @@ export const VPSHostingServicesData = [
         icon: ramMemory,
         name: "64GB",
         subName: "up to 384GB",
+      },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
       },
     ],
   },
@@ -85,12 +147,12 @@ export const VPSHostingServicesData = [
     fullManaged: true,
     title: {
       titleName: "INTEL GOLD 7402P",
-      titleIcon: "",
+      titleIcon: newZealand,
       titleTookit: "Eu-Nord-1:3 in stock",
     },
     subTitle: {
       subTitleName: "Middleweight",
-      subTitleIcon: "",
+      subTitleIcon: newZealand,
       subTitleTookit: "Eu-Nord-1:3 in stock",
     },
     hostingInfo: [
@@ -103,20 +165,39 @@ export const VPSHostingServicesData = [
         icon: ramMemory,
         name: "64GB",
         subName: "up to 384GB",
+      },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
       },
     ],
   },
   {
     id: 5,
-    fullManaged: false,
+    fullManaged: true,
     title: {
       titleName: "INTEL GOLD 7402P",
-      titleIcon: "",
+      titleIcon: newZealand,
       titleTookit: "Eu-Nord-1:3 in stock",
     },
     subTitle: {
       subTitleName: "Middleweight",
-      subTitleIcon: "",
+      subTitleIcon: newZealand,
       subTitleTookit: "Eu-Nord-1:3 in stock",
     },
     hostingInfo: [
@@ -129,6 +210,205 @@ export const VPSHostingServicesData = [
         icon: ramMemory,
         name: "64GB",
         subName: "up to 384GB",
+      },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
+      },
+    ],
+  },
+  {
+    id: 6,
+    fullManaged: false,
+    title: {
+      titleName: "INTEL GOLD 7402P",
+      titleIcon: newZealand,
+      titleTookit: "Eu-Nord-1:3 in stock",
+    },
+    subTitle: {
+      subTitleName: "Middleweight",
+      subTitleIcon: newZealand,
+      subTitleTookit: "Eu-Nord-1:3 in stock",
+    },
+    hostingInfo: [
+      {
+        icon: cpu,
+        name: "26 Cores @ 2.1 GHz",
+        subName: "Benchmark 15,390",
+      },
+      {
+        icon: ramMemory,
+        name: "64GB",
+        subName: "up to 384GB",
+      },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
+      },
+    ],
+  },
+  {
+    id: 7,
+    fullManaged: true,
+    title: {
+      titleName: "INTEL GOLD 7402P",
+      titleIcon: newZealand,
+      titleTookit: "Eu-Nord-1:3 in stock",
+    },
+    subTitle: {
+      subTitleName: "Middleweight",
+      subTitleIcon: newZealand,
+      subTitleTookit: "Eu-Nord-1:3 in stock",
+    },
+    hostingInfo: [
+      {
+        icon: cpu,
+        name: "26 Cores @ 2.1 GHz",
+        subName: "Benchmark 15,390",
+      },
+      {
+        icon: ramMemory,
+        name: "64GB",
+        subName: "up to 384GB",
+      },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
+      },
+    ],
+  },
+  {
+    id: 8,
+    fullManaged: false,
+    title: {
+      titleName: "INTEL GOLD 7402P",
+      titleIcon: newZealand,
+      titleTookit: "Eu-Nord-1:3 in stock",
+    },
+    subTitle: {
+      subTitleName: "Middleweight",
+      subTitleIcon: newZealand,
+      subTitleTookit: "Eu-Nord-1:3 in stock",
+    },
+    hostingInfo: [
+      {
+        icon: cpu,
+        name: "26 Cores @ 2.1 GHz",
+        subName: "Benchmark 15,390",
+      },
+      {
+        icon: ramMemory,
+        name: "64GB",
+        subName: "up to 384GB",
+      },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
+      },
+    ],
+  },
+  {
+    id: 9,
+    fullManaged: false,
+    title: {
+      titleName: "INTEL GOLD 7402P",
+      titleIcon: newZealand,
+      titleTookit: "Eu-Nord-1:3 in stock",
+    },
+    subTitle: {
+      subTitleName: "Middleweight",
+      subTitleIcon: newZealand,
+      subTitleTookit: "Eu-Nord-1:3 in stock",
+    },
+    hostingInfo: [
+      {
+        icon: cpu,
+        name: "26 Cores @ 2.1 GHz",
+        subName: "Benchmark 15,390",
+      },
+      {
+        icon: ramMemory,
+        name: "64GB",
+        subName: "up to 384GB",
+      },
+      {
+        icon: hardDisk,
+        name: "2x 250GB SSD",
+        subName: "up to 8 disks",
+      },
+      {
+        icon: speedometer,
+        name: "3Gbps",
+        subName: "100TB/mo free traffic",
+      },
+      {
+        icon: cable,
+        name: "1",
+        subName: "up to 3",
+      },
+      {
+        icon: priceTag,
+        name: "$14.00/mo",
       },
     ],
   },
